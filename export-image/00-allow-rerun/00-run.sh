1b50ab6a6c79bdb8990ffb97054bdefa5cbf90d7
Andy Jones <3579815+wandering-andy@users.noreply.github.com>
2023-10-16 16:26:09 -0600

    Comments added to 00-run.sh in export-image

--- export-image/00-allow-rerun/00-run.sh
+++ export-image/00-allow-rerun/00-run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 
+# Check if qemu-arm-static executable does not exist in the specified directory
 if [ ! -x "${ROOTFS_DIR}/usr/bin/qemu-arm-static" ]; then
+    # If it does not exist, copy it from /usr/bin/ to the specified directory
 	cp /usr/bin/qemu-arm-static "${ROOTFS_DIR}/usr/bin/"
 fi
 
+# Check if the ld.so.preload file exists in the specified directory
 if [ -e "${ROOTFS_DIR}/etc/ld.so.preload" ]; then
+    # If it exists, rename it to ld.so.preload.disabled
 	mv "${ROOTFS_DIR}/etc/ld.so.preload" "${ROOTFS_DIR}/etc/ld.so.preload.disabled"
 fi
